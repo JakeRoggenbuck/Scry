20bdb9ea3f7d20f2f7988057cb4eb213280b88b2
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-02-15 15:31:42 -0800

    Add network traffic command

--- server/scrycommands.py
+++ server/scrycommands.py
@@ -39,6 +39,22 @@ class LoginSSH:
     invalid_user: bool = False
 
 
+@dataclass
+class TrafficInstance:
+    name: str
+    rx_pkts: int
+    tx_pkts: int
+
+    rx_rate: int
+    tx_rate: int
+
+    rx_pkts_drop: int
+    tx_pkts_drop: int
+
+    rx_over: int
+    tx_coll: int
+
+
 def wrapper(command):
     try:
         output = subprocess.check_output(command, stderr=LOG_FILE, shell=True)
@@ -65,14 +81,24 @@ def netstat_listening_udp():
 
 def failed_login():
     output = wrapper("grep 'Failed password' /var/log/auth.log")
+    # Checks of the command return correctly
     if output is not None:
         output = seperate(output)
         _all = []
         for line in output:
+            # Check for a length 14 variant of the returned output
             if len(line) == 14:
+                # Gets the parts of the line needed to fill the dataclass apart
+                # from the last parameters which has a default to false because
+                # it checks if the login was by a different user and in this
+                # case it will be only length 14, so it defaults to false
                 new = line[0:6] + line[8:9] + line[10:11] + line[12:]
 
+            # Check if the command returned the length 16 variant because it
+            # has the extra (Wrong user) data in the line
             elif len(line) == 16:
+                # Gets the actual pieces of the returned split list, these
+                # things are the parameters needed for the dataclass
                 new = line[0:6] + line[10:11] + line[12:13] + line[14:] + [True]
 
             _all.append(LoginSSH(*new))
@@ -85,6 +111,45 @@ def accepted_login():
         output = seperate(output)
         _all = []
         for x in output:
+            # Adds the needed pieces of the return log file together to input
+            # as parameters to the LoginSSH dataclass
             new = x[0:6] + x[8:9] + x[10:11] + x[12:]
             _all.append(LoginSSH(*new))
         return _all
+
+
+def network_traffic():
+    """
+    #kernel
+    Interface        RX Pkts/Rate    TX Pkts/Rate    RX Data/Rate    TX Data/Rate
+                     RX Errs/Drop    TX Errs/Drop    RX Over/Rate    TX Coll/Rate
+    lo               590 0           590 0           239618 0        239618 0
+                     0 0             0 0             0 0             0 0
+    enp0s31f6        18938 0         6951 0          26403K 0        755268 0
+                     0 0             0 0             0 0             0 0
+    """
+    output = wrapper("ifstat")
+    if output is not None:
+        _all = []
+        # Gets the actual data output without the headers
+        output = seperate(output)[3:]
+        index = 0
+        # This will repeat for each pair of two lines for each interface
+        while index < len(output):
+            # Gets the main data line (Normal traffic)
+            main = output[index]
+            # Gets the name of the interface from the main line
+            name = main[0]
+
+            # Casts each value into an int if it is the first number in the
+            # line, if you take a look at the output, the second number for
+            # each is a is zero so we don't need that, so it checks the mod of
+            # the num (Index) for each value and only adds it to the list if
+            # it's the first item and not the zero
+            traffic = [int(x) for num, x in enumerate(main[1:]) if (num - 1) % 2]
+            dropped = [int(x) for num, x in enumerate(output[index + 1]) if (num - 1) % 2]
+
+            _all.append(TrafficInstance(name, *traffic, *dropped))
+            # Jumps two, to the next interface because each interface has two
+            # lines of output
+            index += 2
