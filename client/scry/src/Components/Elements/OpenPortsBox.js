544ad335b6248c0d2677e3f3368ec1e0227b0a48
George Berdovskiy <georgeberdovskiy@Georges-iMac.local>
2021-02-16 12:01:55 -0800

    Finish Open Ports Box UI

--- client/scry/src/Components/Elements/OpenPortsBox.js
+++ client/scry/src/Components/Elements/OpenPortsBox.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import ClickToOpen from '../../Assets/ClickToOpen.png'
 
 const OpenPortsBox = (props) => {
     
@@ -8,6 +9,8 @@ const OpenPortsBox = (props) => {
             <Link className="disguised-a" to="/open-ports">
                 <div className="monitor-box-div-card">
                     <h3>Open Ports</h3>
+
+                    <img className="open-ports-click-img" src={ClickToOpen}/>
                 </div>
             </Link>
         </div>
